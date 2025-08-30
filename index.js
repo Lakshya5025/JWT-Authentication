e666f0e7634a4cd24848d33d2a5f49919108248a
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-08-30 15:16:02 +0530

    Removed token generating function

--- index.js
+++ index.js
@@ -6,23 +6,7 @@ const JWT_SECRET = 'hellomynameislakshyagupta';
 
 let users = [];
 let loggedInIdx = [];
-const generateToken = function () {
-    let token = "";
-    const values = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-        'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2',
-        '3', '4', '5', '6', '7', '8', '9'
-    ]
-    const len = values.length;
-    for (let i = 0; i < 32; i++) {
-        let randValue = values[Math.round(Math.random() * len)];
-        token += randValue;
-    }
-    return token;
-}
+
 
 const signupHandler = function (req, res) {
     // console.log(req);
@@ -58,11 +42,9 @@ const signinHandler = function (req, res) {
             users[e].loggedIn = false;
         })
         loggedInIdx = [];
-        message = generateToken();
         const jwtToken = jwt.sign({ username: foundAlreadyFound.username }, JWT_SECRET);
         // console.log(jwtToken);
         users[foundIdx].jwtToken = jwtToken
-        users[foundIdx].token = message;
         users[foundIdx].loggedIn = true;
         loggedInIdx.push(foundIdx);
         message = jwtToken;
